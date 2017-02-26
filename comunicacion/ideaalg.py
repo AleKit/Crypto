f90b41170d95cef6564edb663c5185feaa93117a
AleKit <alagui01@ucm.es>
2017-02-26 18:44:38 +0100

    descifrador
    (de momento hay algo mal)

--- comunicacion/ideaalg.py
+++ comunicacion/ideaalg.py
@@ -1,6 +1,6 @@
 def generadorclave(clave):
     z = []
-    while len(z) <= 52:
+    while len(z) < 52:
         z.append(clave[:16])
         z.append(clave[16:32])
         z.append(clave[32:48])
@@ -10,19 +10,43 @@ def generadorclave(clave):
         z.append(clave[96:112])
         z.append(clave[112:128])
         
-        clave = clave[24:] + clave[:24]
+        clave = clave[25:] + clave[:25]
+        
+    while len(z) > 52:
+        z.pop()
     return z
 
+def generadordecr(z):
+    w = []
+    j = 48
+    w.append(inversomodulo(z[j]))
+    w.append(opuestomodulo(z[j+1]))
+    w.append(opuestomodulo(z[j+2]))
+    w.append(inversomodulo(z[j+3]))
+    while len(w) < 52:
+        w.append(z[j-2])
+        w.append(z[j-1])
+        #print(w)
+        j -= 6
+        w.append(inversomodulo(z[j]))
+        #print j
+        w.append(opuestomodulo(z[j+2]))
+        w.append(opuestomodulo(z[j+1]))
+        w.append(inversomodulo(z[j+3]))
+        
+    return w
 
-def ideaalg(clave,texto):
+def ideaalg(clave,texto,encriptar = 1):
     z = generadorclave(clave)
-    #print z
+    if encriptar == 0:
+        z = generadordecr(z)
+    print z
     x1 = texto[:16]
     x2 = texto[16:32]
     x3 = texto[32:48]
     x4 = texto[48:64]
     j = 0
-    for i in xrange(8):          
+    for i in xrange(8):
         g1 = multimodulo(x1,z[j])
         j += 1
         g2 = sumamodulo(x2,z[j])
@@ -31,24 +55,28 @@ def ideaalg(clave,texto):
         j += 1
         g4 = multimodulo(x4,z[j])
         j += 1
-        g5 = xor(g1,g3) #paso 5
-        g6 = xor(g2,g4) #paso 6
+        g5 = xor(g1,g3) 
+        g6 = xor(g2,g4) 
         
         g7 = multimodulo(g5,z[j])
         j += 1 
-        g8 = sumamodulo(g6,g7) #paso 8
+        g8 = sumamodulo(g6,g7) 
         g9 = multimodulo(g8,z[j])
-        j += 1 #paso 9
-        g10 = sumamodulo(g7,g9) #paso 10
+        j += 1 
+        g10 = sumamodulo(g7,g9) 
         g11 = xor(g1,g9)
         g12 = xor(g3,g9)
         g13 = xor(g2,g10)
         g14 = xor(g4,g10)
-        
+
         x1 = g11
-        x2 = g13
-        x3 = g12
         x4 = g14
+        if i < 7:
+            x2 = g13
+            x3 = g12
+        elif i == 7:
+            x2 = g12
+            x3 = g13        
         
     g1 = multimodulo(x1,z[j])
     j += 1
@@ -57,5 +85,5 @@ def ideaalg(clave,texto):
     g3 = sumamodulo(x3,z[j])
     j += 1
     g4 = multimodulo(x4,z[j])
-    #print j
+    print j
     return g1 + g2 + g3 + g4
