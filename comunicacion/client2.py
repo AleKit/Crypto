a39a3f653c0d85b00be02d504d9b5f0a2065753a
AleKit <alagui01@ucm.es>
2017-04-08 20:46:10 +0200

    RSA, ElGamal
    ejemplos wiki

--- comunicacion/client2.py
+++ comunicacion/client2.py
@@ -12,6 +12,27 @@ import random
 address = ('localhost', 6000)
 conn = Client(address, authkey='secret password')
 
+msg1 = conn.recv()
+
+if msg1[:3] == 'RSA':
+    m_rsa = 123
+    comma = msg1.index(',')
+    encr_rsa = modular_pow(m_rsa,int(msg1[3:comma]),int(msg1[comma+1:]))
+    conn.send('RSAe' + str(encr_rsa))
+    
+if msg1[:2] == 'EG':
+    comma = msg1.index(',')
+    semicolon = msg1.index(';')
+    m_gamal = 128688
+    b_gamal = 480
+    y1_gamal = modular_pow(int(msg1[2:comma]),b_gamal,
+                           int(msg1[comma+1:semicolon]))
+    y2_gamal = (modular_pow(int(msg1[semicolon+1:]),b_gamal,
+                           int(msg1[comma+1:semicolon])) * m_gamal
+                           ) % int(msg1[comma+1:semicolon])
+    conn.send('EGe' + str(y1_gamal) + ',' + str(y2_gamal))
+    
+
 p = 2**128-159
 g = 127696581778553318510832226979062695964
 b = p
@@ -19,7 +40,7 @@ while b > p-1:
     bstr = numerogrande(int(round(random.random()*10)+10))
     b = int(bstr,2)
 #B = g**b % p
-B = modular_power(g,b,p)
+B = modular_pow(g,b,p)
 
 message = 'B' + str(B)
 
@@ -30,8 +51,9 @@ if msg[0] == 'A':
     message2 = msg[1:]
 
 A = int(message2)
+print A
 #s = A**b % p
-s = modular_power(A,b,p)
+s = modular_pow(A,b,p)
 print s
 
 cifrador = 'prueba'
