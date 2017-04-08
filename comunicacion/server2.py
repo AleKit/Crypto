fbe98fa897cc8e636216c0ba1ae71a3bbcd4e068
AleKit <alagui01@ucm.es>
2017-04-08 20:47:10 +0200

    RSA, ElGamal
    ejemplos wiki

--- comunicacion/server2.py
+++ comunicacion/server2.py
@@ -9,11 +9,43 @@ from multiprocessing.connection import Listener
 import cripto1
 import random
 
+#RSA
+usersa = input("RSA yes or no? yes = 1, no = 0: ")
+if usersa == 1:
+    p_rsa = 61
+    q_rsa = 53
+    n_rsa = p_rsa*q_rsa
+    e_rsa = 17
+    d_rsa = int(inversomodulo(bin(e_rsa),(p_rsa-1)*(q_rsa-1)-1),2)
+   
+#ElGamal
+useElGamal = input("ElGamal yes or no? yes = 1, no = 0: ")
+if useElGamal == 1:
+    p_gamal = 15485863
+    g_gamal = 7
+    a_gamal = 21702
+    K_gamal = modular_pow(g_gamal, a_gamal, p_gamal)
+
+
+
 address = ('localhost', 6000)     # family is deduced to be 'AF_INET'
 listener = Listener(address, authkey='secret password')
 conn = listener.accept()
 print 'connection accepted from', listener.last_accepted
 a = -1 #esto hay que cambiarlo quiza
+
+if usersa == 1:
+    rsamessage = 'RSA' + str(e_rsa) + ',' + str(n_rsa)
+    print rsamessage
+    conn.send(rsamessage)
+
+if useElGamal == 1:
+    gamalmessage = 'EG' + str(g_gamal) + ',' + str(p_gamal) + ';' + str(K_gamal)
+    print gamalmessage
+    conn.send(gamalmessage)
+    
+B = 0
+
 while True:
     msg = conn.recv()
     print msg
@@ -29,22 +61,30 @@ while True:
             astr = numerogrande(int(round(random.random()*10)+10))
             a = int(astr,2)
         #A = g**a % p
-        A = modular_power(g,a,p)
+        A = modular_pow(g,a,p)
         message = 'A' + str(A)
         conn.send(message)
         B = int(msg[1:])
         #s = B**a % p
-        s = modular_power(B,a,p)
+        s = modular_pow(B,a,p)
         print s
-    elif B and msg[0:3] == 'cif':
+    elif B > 0 and msg[:3] == 'cif':
         descifrador = msg[3:]
-    elif B and msg[0:3] == 'vig':
+    elif B > 0 and msg[:3] == 'vig':
         text = cripto1.decr_vigenere(msg[3:],descifrador)
         print text
-    elif B and msg[0:4] == 'idea':
+    elif B > 0 and msg[:4] == 'idea':
         e = bin(s) 
         f = int(e,2)
         claveidea = '{0:0{1}b}'.format(f,128)
         descifradoidea = ideaalg(clave,msg[4:],0)
+    elif msg[:4] == 'RSAe':
+        decrypt_rsa = modular_pow(int(msg[4:]),d_rsa,n_rsa)
+        print decrypt_rsa
+    elif msg[:3] == 'EGe':
+        comma = msg.index(',')
+        decrypt_gamal = (modular_pow(int(msg[3:comma]),p_gamal-1-a_gamal, 
+                                        p_gamal) * int(msg[comma+1:])) % p_gamal
+        print decrypt_gamal
         
 listener.close()
